8cb3538df87cb4fd4d4424ee00a1adacbec004ff
emily <jej001@ucsd.edu>
2017-10-31 18:50:12 -0700

    Validation + Setting modal

--- js/schedule.js
+++ js/schedule.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
-  
+  var form = $("form#needs-validation");
+  $(form).submit(addExercise);
 });
 
 function addExercise(e) {
+  e.preventDefault();
   bootbox.alert({
       size: "large",
       message: "Your new exercise has been scheduled!",
