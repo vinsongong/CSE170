eb5ec0c1ff5e5440a14f97ef38a7dd7681ebbafc
emily <jej001@ucsd.edu>
2017-10-31 17:59:01 -0700

    Updated Schedule.html with validation

--- js/schedule.js
+++ js/schedule.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
-  $("button.save").click(addExercise);
+  
 });
 
 function addExercise(e) {
-  e.preventDefault();
   bootbox.alert({
       size: "large",
       message: "Your new exercise has been scheduled!",
