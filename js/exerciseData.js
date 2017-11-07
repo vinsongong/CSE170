9e692d5c7fe54dcace6385c9c43f1298fe407b9d
emily <jej001@ucsd.edu>
2017-11-07 11:44:24 -0800

    Added JSON for exercise data

---
+++ js/exerciseData.js
@@ -0,0 +1,38 @@
+var shoulderRaise = {
+	exerciseName:"Shoulder Raises",
+	distraction:"Medium",
+	duration:{
+		time:"10",
+		unit:"minutes"
+	},
+	isRecommended:"true",
+	description:["Do 2 sets of 10", "Can multitask with work"],
+	equipment:"Weights or dumbells",
+	youtubeLink:"https://www.youtube.com/embed/q5sNYB1Q6aM"
+}
+
+var jumpingJacks = {
+	exerciseName:"Jumping Jacks",
+	distraction:"High",
+	duration:{
+		time:"15",
+		unit:"minutes"
+	},
+	isRecommended:"true",
+	description:["Do 3 sets of 10", "Requires full attention"],
+	equipment:"None",
+	youtubeLink:"https://www.youtube.com/embed/UpH7rm0cYbM"
+}
+
+var standing = {
+	exerciseName:"Standing",
+	distraction:"Low",
+	duration:{
+		time:"1",
+		unit:"hours"
+	},
+	isRecommended:"false",
+	description:["Standing up while working", "Can do this while studying"],
+	equipment:"None",
+	youtubeLink:""
+}
