4ff0f2a876b516cdf2ff3d00003422541fc0001f
emily <jej001@ucsd.edu>
2017-11-07 13:57:18 -0800

    Updated the option list for ScheduleExercise

--- js/schedule.js
+++ js/schedule.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+
+  var retrievedObject = localStorage.getItem('exerciseData');
+  var exerciseArray = JSON.parse(retrievedObject);
+
+  /* Handlers bar */
+  var optionSource = $("#option-template").html();
+  var optionTemplate = Handlebars.compile(optionSource);
+  var optionHtml = optionTemplate(exerciseArray);
+  $("#select-exercise").append(optionHtml);
+
   var form = $("form#needs-validation");
   $(form).submit(addExercise);
 });
