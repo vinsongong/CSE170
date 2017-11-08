ac9ebcdb60a972dcea6a98aee8d55bfb1fe45ce8
Vinson Gong <vgong@ucsd.edu>
2017-11-07 19:52:31 -0800

    In progress modify mySchedule

--- js/mySchedule.js
+++ js/mySchedule.js
@@ -47,15 +47,54 @@
         start.setHours(startArray[0]);
         start.setMinutes(startArray[1]);
         start.setSeconds(0);
-        
+
         while ((start.getTime() - currMillis) <= 0){
              start.setTime(start.getTime() + timePeriodMillis);
         }
 
-        //If the timer has pass 
+        //If the timer has pass
         console.log(start);
 
         $(startTime).countdown({until: start, format: 'dHMS'});
     });
 
-}); 
+    $("button.modify").click(modifyScheduleItem);
+    $("button.cancel").click();
+    $("button.save").click();
+    $("button.delete").click();
+});
+
+function modifyScheduleItem(e) {
+    e.preventDefault();
+    $(this).hide();
+    $(this).parent().find(".delete").show();
+    $(this).parent().find(".save").show();
+    $(this).parent().find(".cancel").show();
+    var modalDetailsDiv = $(this).parents().eq(1).find(".modal-details");
+    modalDetailsDiv.hide();
+
+    var modalBody = $(this).parents().eq(1).find(".modal-body");
+
+    //Excercise Name
+    var exerciseName = $(this).parents().eq(1).find(".modal-title").text();
+    var exerciseCode = "<div class='scheduleExerciseNameDiv'>Exercise Name:<br /><input" +
+    " type='text' class='scheduleExerciseNameInput form-control' value='" +
+    exerciseName + "' autofocus required/></div>";
+    modalBody.append(exerciseCode);
+
+    //Repeats Every
+    var repeatTimeAndUnits = modalDetailsDiv.find(".modal-repeat").text().split(" ");
+    var repeatTime = repeatTimeAndUnits[2];
+    var repeatUnits = repeatTimeAndUnits[3];
+    var repeatTimeCode = "<div class='repeatDiv'>Repeat Every:<br /><input type='number' class='repeatVal form-control' " +
+    "placeholder='' min='1' max='60' value='" + repeatTime + "' />";
+    var repeatUnitsCode = "<select class='repeatUnitsDrop form-control'>" +
+    "<option value='seconds'>seconds</option>" +
+    "<option value='minutes'>minutes</option>" +
+    "<option value='hours'>hours</option>" +
+    "<option value='days'>days</option>" +
+    "</select></div>";
+    modalBody.append(repeatTimeCode + repeatUnitsCode);
+    $(".repeatUnitsDrop option[value=" + repeatUnits+ "]").attr("selected", true);
+
+}
